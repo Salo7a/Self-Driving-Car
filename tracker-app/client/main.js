30b8b478cbb2028b74751698c3edd5957402128f
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-11-24 00:32:07 +0200

    Update main.js - Remove trash

--- tracker-app/client/main.js
+++ tracker-app/client/main.js
@@ -1,5 +1,3 @@
-import { MeteorCameraUI } from 'meteor/okland:camera-ui';
-
 import './main.html';
 
 // let zeroconf = cordova.plugins.zeroconf;
@@ -64,46 +62,17 @@ Meteor.startup(function() {
   }
 });
 
-
-
 if (Meteor.isServer) {
   console.log("Printed on the server");
 }
 
 if (Meteor.isClient) {
   console.log("Printed in browsers and mobile apps");
-
-  // Template.takePhoto.events({
-  //   'click .capture': function(){
-  //     console.log("Button clicked.");
-      
-  //     MeteorCameraUI.getPicture({width: 500, height: 500, quality: 50}, function(error, data) {
-  //         if (error) {
-  //           throw error;
-  //         }
-
-  //         let image = document.getElementById('myImage');
-  //         image.src = data;
-  //     });
-
-  //   }
-  // });
-  
 }
 
 if (Meteor.isCordova) {
   console.log("Printed only in mobile Cordova apps");
 
-  Template.takePhoto.events({
-    'click .capture2': function () {
-        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-          destinationType: Camera.DestinationType.DATA_URL
-        });
-    }
-  });
-
-  
-
   //     MeteorCameraUI.getPicture({width: 500, height: 500, quality: 50}, function(error, data) {
   //       if (error) {
   //         throw error;
@@ -112,38 +81,5 @@ if (Meteor.isCordova) {
   //       let image = document.getElementById('myImage');
   //       image.src = data;
   //     });
-
-  // Template.takePhoto.events({
-  //   'click .capture': function(event){
-  //     console.log("Button clicked.");
-
-  //     MeteorCameraUI.getPicture({width: 500, height: 500, quality: 50}, function(error, data) {
-  //       if (error) {
-  //         throw error;
-  //       }
-
-  //       let image = document.getElementById('myImage');
-  //       image.src = data;
-  //     });
-
-  //     // MeteorCamera.getPicture({width: 300, height: 300, quality: 100}, function(error, data) {
-  //     //   console.log(data);
-  //     //   let image = document.getElementById('myImage');
-  //     //   // image.src = "data:image/jpeg;base64," + data;
-  //     //   image.src = data;
-  //     //   // Session.set('photo', data);
-  //     // });
-  //   }
-  // });
-
 }
 
-
-function onSuccess(imageData) {
-  let image = document.getElementById('myImage');
-  image.src = imageData;
-}
-
-function onFail(message) {
-  alert('Failed because: ' + message);
-}
