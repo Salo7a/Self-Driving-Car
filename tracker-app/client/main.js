8071fad060812cb072de69cdab0ae30505e56966
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-11-24 01:04:13 +0200

    Update main.js

--- tracker-app/client/main.js
+++ tracker-app/client/main.js
@@ -1,8 +1,5 @@
 import './main.html';
 
-// let zeroconf = cordova.plugins.zeroconf;
-// zeroconf.watchAddressFamily = 'ipv4';
-
 // code to run on server at startup
 Meteor.startup(function() {
   if (Meteor.isCordova) 
@@ -29,36 +26,6 @@ Meteor.startup(function() {
       }, function(error) {
           console.error("The following error occurred: " + error);
       }, cordova.plugins.diagnostic.permission.CAMERA);
-
-      // Configure zeroconf
-      // zeroconf.watch('_http._tcp.', 'local.', function(result) {
-      //     let action = result.action;
-      //     let service = result.service;
-      //     if (action == 'added')
-      //     {
-      //         console.log('service added', service);
-      //     }
-      //     else if (action == 'resolved')
-      //     {
-      //         console.log('service resolved', service);
-      //         $("p").text = service;
-      //         /* service : {
-      //         'domain' : 'local.',
-      //         'type' : '_http._tcp.',
-      //         'name': 'Becvert\'s iPad',
-      //         'port' : 80,
-      //         'hostname' : 'ipad-of-becvert.local',
-      //         'ipv4Addresses' : [ '192.168.1.125' ],
-      //         'ipv6Addresses' : [ '2001:0:5ef5:79fb:10cb:1dbf:3f57:feb0' ],
-      //         'txtRecord' : {
-      //             'foo' : 'bar'
-      //         } */
-      //     }
-      //     else {
-      //         console.log('service removed', service);
-      //     }
-      // });
-
   }
 });
 
