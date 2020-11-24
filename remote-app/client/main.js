7df0cc0008e4f9719b59583a1f1c7d2fae443ae0
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-11-24 14:57:40 +0200

    Edits

--- remote-app/client/main.js
+++ remote-app/client/main.js
@@ -2,12 +2,10 @@ import { Template } from 'meteor/templating';
 
 import './main.html';
 
-// let zeroconf = cordova.plugins.zeroconf;
-// zeroconf.watchAddressFamily = 'ipv4';
-
-// const serviceDiscovery = require("../plugins/cordova-plugin-discovery/www/serviceDiscovery");
-let WifiWizard2 = null;
+let zeroconf = null;
+let serviceDiscovery = null;
 let ESP_MAC = '';
+let ESP_SSID = '';
 let ESP_IP = '';
 
 Meteor.startup(function() {
@@ -22,50 +20,10 @@ Meteor.startup(function() {
         function onDeviceReady() {
             console.log("Device is ready now!");
             // Now safe to use device APIs
-            WifiWizard2 = require("../plugins/cordova-plugin-wifiwizard2/www/WifiWizard2");
-            // let WifiManager = require("../plugins/cordova-plugin-android-wifi-manager/www/index");
-            // let zeroconf = require("../plugins/cordova-plugin-zeroconf/www/zeroconf");
-            // zeroconf.watchAddressFamily = 'ipv4';
-            //
-            // // Configure zeroconf
-            // zeroconf.watch('_http._tcp.', 'local.', function(result) {
-            //     let action = result.action;
-            //     let service = result.service;
-            //     if (action === 'added')
-            //     {
-            //         console.log('service added', service);
-            //     }
-            //     else if (action === 'resolved')
-            //     {
-            //         console.log('service resolved', service);
-            //         $("p").text = service;
-            //         /* service : {
-            //         'domain' : 'local.',
-            //         'type' : '_http._tcp.',
-            //         'name': 'Becvert\'s iPad',
-            //         'port' : 80,
-            //         'hostname' : 'ipad-of-becvert.local',
-            //         'ipv4Addresses' : [ '192.168.1.125' ],
-            //         'ipv6Addresses' : [ '2001:0:5ef5:79fb:10cb:1dbf:3f57:feb0' ],
-            //         'txtRecord' : {
-            //             'foo' : 'bar'
-            //         } */
-            //     }
-            //     else {
-            //         console.log('service removed', service);
-            //     }
-            // });
-
-            // let serviceType = "ssdp:all";
-            // let success = function(devices) {
-            //     console.log(devices);
-            // }
-            //
-            // let failure = function() {
-            //     alert("Error calling Service Discovery Plugin");
-            // }
-            //
-            // serviceDiscovery.getNetworkServices(serviceType, success, failure);
+            zeroconf = cordova.plugins.zeroconf;
+            zeroconf.watchAddressFamily = 'ipv4';
+            // serviceDiscovery = cordova.plugins.serviceDiscovery;
+            // serviceDiscovery = require("../plugins/cordova-plugin-discovery/www/serviceDiscovery");
         }
     }
 });
@@ -74,48 +32,61 @@ if (Meteor.isCordova) {
     Template.ConnectESP.events({
         'click #connectESP' (event, instance) {
             console.log("ESP Clicked");
-            WifiWizard2.requestPermission().then(r => console.log(r))
-                .catch(e => console.log(e));
-
-            WifiWizard2.getConnectedBSSID().then(r => console.log(r))
-                .catch(e => console.log(e));
-
-            WifiWizard2.getWifiIP().then(r => console.log("STUDBME2 IP " + r))
-                .catch(e => console.log(e));
-
-            // WifiWizard2.listNetworks().then(r => console.log(r))
-            //     .catch(e => console.log(e));
-
-            WifiWizard2.scan({}).then(function (results) {
-                // console.log(results);
-                WifiWizard2.getScanResults({}).then((networks) => {
-                    // console.log(networks);
-                    networks.forEach(net => {
-                        console.log(net["SSID"]);
-                        console.log(net["BSSID"]);
-                        if (net["BSSID"] === ESP_MAC) {
-                            // ESP_IP = net["BSSID"];
-                        }
-                    });
-                }).catch((e) => {
-                    console.log("Error ", e);
-                });
-            }).catch(e => console.log( 'Error getting results!', e ));
 
             // let serviceType = "ssdp:all";
+
             // let success = function(devices) {
-            //   console.log(devices);
+            //     console.log(devices);
             // }
-            //
+
             // let failure = function() {
             //     alert("Error calling Service Discovery Plugin");
             // }
-            //
-            // serviceDiscovery.getNetworkServices(serviceType, true, success, failure);
+            // //
+            // // /**
+            // //  * Similar to the W3C specification for Network Service Discovery api 'http://www.w3.org/TR/discovery-api/'
+            // //  * @method getNetworkServices
+            // //  * @param {String} serviceType e.g. "urn:schemas-upnp-org:service:ContentDirectory:1", "ssdp:all", "urn:schemas-upnp-org:service:AVTransport:1"
+            // //  * @param {Function} success callback an array of services
+            // //  * @param {Function} failure callback
+            // //  */
+            // serviceDiscovery.getNetworkServices(serviceType, success, failure);
+            // console.log("Done");
 
-            WifiWizard2.connect("StudBME1");
-            WifiWizard2.getWifiIP().then(r => console.log("StudBME1 IP " + r))
-                .catch(e => console.log(e));
+            // Configure zeroconf
+            zeroconf.watch('_http._tcp.', 'local.', function(result) {
+                let action = result.action;
+                let service = result.service;
+                if (action === 'added')
+                {
+                    console.log('service added', service);
+                    console.log(service["domain"] + " : "+ service["hostname"] + " : " + service["ipv4Addresses"]);
+                    if (service["hostname"] === ESP_SSID) {
+                        ESP_IP = service["ipv4Addresses"];
+                    }
+                }
+                else if (action === 'resolved')
+                {
+                    console.log('service resolved', service);
+                    console.log(service["domain"] + " : "+ service["hostname"] + " : " + service["ipv4Addresses"]);
+                    /* service : {
+                    'domain' : 'local.',
+                    'type' : '_http._tcp.',
+                    'name': 'Becvert\'s iPad',
+                    'port' : 80,
+                    'hostname' : 'ipad-of-becvert.local',
+                    'ipv4Addresses' : [ '192.168.1.125' ],
+                    'ipv6Addresses' : [ '2001:0:5ef5:79fb:10cb:1dbf:3f57:feb0' ],
+                    'txtRecord' : {
+                        'foo' : 'bar'
+                    } */
+                }
+                else {
+                    console.log('service removed', service);
+                }
+            });
+
+
         }
     });
 }
