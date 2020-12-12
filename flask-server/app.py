99e0b21fe2c52990ef83433eb4fa4ee86acb1cd2
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-12-13 00:58:28 +0200

    Added Flask Server

---
+++ flask-server/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request, after_this_request
+from lanedetection import *
+
+app = Flask(__name__)
+
+lowerColors, upperColors = ..., ...
+color_name = "blue"
+
+# Choose Colors Range
+if color_name == "white":
+    # range of white in HSV
+    lowerColors = np.array([0, 0, 168])
+    upperColors = np.array([172, 111, 255])
+elif color_name == "blue":
+    # Range of Blue in HSV
+    lowerColors = np.array([60, 40, 40])
+    upperColors = np.array([150, 255, 255])
+
+@app.route('/')
+def main():
+    return "main Page"
+
+
+@app.route('/detect/<path:frameURI>')
+def process(frameURI):
+
+    @after_this_request
+    def add_header(response):
+        # To allow CORS (Cross Origin Resource Sharing)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+
+    # frameURI = request.args.get('img_uri')
+    # color_name = request.args.get('name')
+
+    frameTest = data_uri_to_cv2_img(frameURI)
+    laneLines = detectLane(frameTest, lowerColors, upperColors)
+    laneLinesImage = displayLines(frameTest, laneLines)
+
+    steeringAngle = computeSteeringAngle(frameTest, laneLines)
+    finalImage = displayHeadingLine(laneLinesImage, steeringAngle)
+    finalImageRGB = convert_BGR_to_RGB(finalImage)
+    finalImageRGBBase64 = img_to_data_uri(finalImageRGB)
+
+    showImage(finalImageRGB, "Final Image in RGB Lanes & Route")
+
+    resultJSON = {"angle": steeringAngle, "imgBase": finalImageRGBBase64}
+    print(resultJSON["angle"])
+    return jsonify(resultJSON)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
