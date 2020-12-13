3e54e1eeae93fc3e77821facfd49199d090f15ef
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-12-13 22:43:06 +0200

    Fix URL Encoding

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request, after_this_request
+import urllib.parse
 from lanedetection import *
+
 
 app = Flask(__name__)
 
@@ -16,22 +18,26 @@ elif color_name == "blue":
     lowerColors = np.array([60, 40, 40])
     upperColors = np.array([150, 255, 255])
 
+
 @app.route('/')
 def main():
     return "main Page"
 
 
-@app.route('/detect/<path:frameURI>')
-def process(frameURI):
+# @app.route('/detect/<path:frameURI>')
+# def process(frameURI):
 
+@app.route('/detect')
+def process():
     @after_this_request
     def add_header(response):
         # To allow CORS (Cross Origin Resource Sharing)
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
 
-    # frameURI = request.args.get('img_uri')
-    # color_name = request.args.get('name')
+    color = request.args.get('color')
+    frameURI = request.args.get('frame_uri')
+    print(color)
 
     frameTest = data_uri_to_cv2_img(frameURI)
     laneLines = detectLane(frameTest, lowerColors, upperColors)
