4e1af9b28735a96f18c89a2554f62b88d877d08c
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-12-14 04:21:16 +0200

    Remove old stuff

--- remote-app/server/main.js
+++ remote-app/server/main.js
@@ -1,83 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 
-const {PythonShell} = require('python-shell');
-
 let ESP_IP;
-let angle;
-let direction;
-let file_path = 'D:/Study/Courses/College/Electronics-Tasks-4th-Year/task3-sbe403a_f20_task3_03/remote-app/server/lanedetection.py'
-
 
 Meteor.startup(() => {
     // code to run on server at startup
 
+    // Methods to be called from client-side
     Meteor.methods({
-      getStream(mStream) {
-        console.log("mediaStream From Server", mStream);
-      },
-
       sendESPIP(espIP) {
         ESP_IP = espIP;
         console.log(ESP_IP);
       },
-
-
-      sendImgURI(frame) {
-          // let frameData = frame.imgSrc;
-          // console.log("frame is: ", frame[0]);
-          // console.log("tag is: ", frame[1]);
-          // let frameTag = frame.imgTag;
-          // console.log("frameData: ", frame);
-          // console.log("screenShotTag: ", frameTag);
-          let res = 5;
-          let angle;
-          let updated_frame;
-
-          let options = {
-              mode: 'text',
-              pythonPath: 'C:/Users/Refaey/AppData/Local/Programs/Python/Python37/python.exe',
-              pythonOptions: ['-u'],
-              // scriptPath: './lanedetection.py',
-              args: frame
-          };
-
-
-          PythonShell.run(file_path, options, function (err, results) {
-            if (err) throw err;
-            console.log('results: %j', results);
-
-            // results is an array consisting of messages collected during execution
-            // Some Conditions to choose direction
-            // direction = '/forward';
-            // console.log("Adding results to res");
-           
-            res = results;
-
-            // angle = results[0]
-            // updated_frame = results[1]
-          });
-
-          // let pyshell = new PythonShell(file_path, options);
-          // pyshell.send('Hello');
-
-          // pyshell.on('message', function (message) {
-          //   // received a message sent from the Python script (a simple "print" statement)
-          //   console.log(message);
-          // });
-
-          // // end the input stream and allow the process to exit
-          // pyshell.end(function (err, code, signal) {
-          //   if (err) throw err;
-          //   console.log('The exit code was: ' + code);
-          //   console.log('The exit signal was: ' + signal);
-          //   console.log('finished');
-          // });
-
-          // console.log("returning res");
-          return res;
-          
-      },
-
     });
-
 });
